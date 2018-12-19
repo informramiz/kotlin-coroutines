8e530b8cdf30c554ac778843e4f3532bb207be35
Ramiz Raja <informramiz@gmail.com>
2018-12-19 08:57:26 +0500

    feat: Intial commit

---
+++ app/src/main/java/com/informramiz/github/kotlincoroutines/ui/main/MainViewModel.kt
@@ -0,0 +1,22 @@
+package com.informramiz.github.kotlincoroutines.ui.main
+
+import android.arch.lifecycle.ViewModel
+import android.util.Log
+import kotlinx.coroutines.*
+
+class MainViewModel : ViewModel() {
+    private val viewModelJob = Job()
+    private val viewModelScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+    fun printAfterDelay() {
+        viewModelScope.launch {
+            delay(1000) //suspend for 1 second
+            Log.i(MainViewModel::class.java.simpleName, "1 second passed")
+        }
+    }
+}
